a15994b99c5ecd56b9fe58b7cad82f6f56f9215c
OHSUJIN <tnwls2dkdlel@naver.com>
2024-11-25 09:51:38 +0900

    프로젝트 삭제

--- React/apa/src/layouts/MainLogo.jsx
+++
@@ -1,9 +0,0 @@
-const MainLogo = () => {
-  return (
-    <div className="main-logo">
-      <img className="logo" src="/logo.png"></img>
-    </div>
-  );
-};
-
-export default MainLogo;
--- React/apa/src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/authSlice';
-
-const rootReducer = combineReducers({
-    auth: authReducer
-});
-
-export default rootReducer;
